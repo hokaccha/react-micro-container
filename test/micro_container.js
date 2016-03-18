6eb59d5042d3805ecd18597f06d09386121baef8
Kazuhito Hokamura <k.hokamura@gmail.com>
2016-03-18 22:22:34 +0900

    Add test

---
+++ test/micro_container.js
@@ -0,0 +1,34 @@
+import assert from 'power-assert';
+import sinon from 'sinon';
+import React from 'react';
+import MicroContainer from '../lib/micro_container';
+
+describe('MicroContainer', () => {
+  let spy;
+  let container;
+  class Container extends MicroContainer {}
+
+  beforeEach(() => {
+    spy = sinon.spy();
+    container = new Container();
+    container.subscribe({ foo: spy });
+    container.dispatch('foo', 'bar', 'baz');
+  });
+
+  it('should inherit React.Component', () => {
+    assert(container instanceof React.Component);
+  });
+
+  it('should be called registered function', () => {
+    assert(spy.callCount === 1);
+  });
+
+  it('`this` value should be container', () => {
+    assert(spy.thisValues[0] === container);
+  });
+
+  it('should receive arguments', () => {
+    assert(spy.args[0][0] === 'bar');
+    assert(spy.args[0][1] === 'baz');
+  });
+});
